cb4edcddf90dbd3c489454e00fb08f030bbd699d
github username <github email address>
2014-07-07 19:18:29 -0400

    Add Firebase JS assets and create reading-list-seed.json to upload on Firebase.

--- js/app.js
+++ js/app.js
@@ -86,13 +86,18 @@ App.Book = DS.Model.extend({
 	review: DS.attr(),
 	rating: DS.attr('number'),
 	amazon_id: DS.attr(),
-	genre: DS.belongsTo('genre'),
+	genre: DS.belongsTo('genre', {async: true}),
 	url: function() {
 		return "http://www.amazon.com/gp/product/"+this.get('amazon_id');
 	}.property('amazon_id'),
 	image: function() {
 		return "http://images.amazon.com/images/P/"+this.get('amazon_id')+".01.ZTZZZZZZ.jpg";
 	}.property('amazon_id')
+});
+
+App.Genre = DS.Model.extend({
+	name: DS.attr(),
+	books: DS.hasMany('book', {async: true})
 });
 
 // App.Book.FIXTURES = [
@@ -124,11 +129,6 @@ App.Book = DS.Model.extend({
 //     genre: 3
 //   }
 // ];
-
-// App.Genre = DS.Model.extend({
-// 	name: DS.attr(),
-// 	books: DS.hasMany('book', {async: true})
-// });
 
 // App.Genre.FIXTURES = [
 //   {
